27bb01f0310fca4bac6263c64f262b87441c1488
Martin Mydliar <mydliar6@stud.uniza.sk>
2025-01-08 15:00:20 +0100

    Prva funkcna verzia hadika s pipami

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mydliar6/snake/src/pipe.c" "CMakeFiles/server.dir/src/pipe.c.o" "gcc" "CMakeFiles/server.dir/src/pipe.c.o.d"
+  "/home/mydliar6/snake/src/server.c" "CMakeFiles/server.dir/src/server.c.o" "gcc" "CMakeFiles/server.dir/src/server.c.o.d"
+  "/home/mydliar6/snake/src/snake_game.c" "CMakeFiles/server.dir/src/snake_game.c.o" "gcc" "CMakeFiles/server.dir/src/snake_game.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
